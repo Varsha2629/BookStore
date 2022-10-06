82fb0e9c388fef27911bec7876b03d3f04b48aad
Varsha2629 <varsha3051@gmail.com>
2022-10-05 21:22:05 -0400

    delete lnk and update fun in AuthServiceTest

--- src/test/java/com/fdmgroup/bookstore/test/AuthenticationServiceTest.java
+++ src/test/java/com/fdmgroup/bookstore/test/AuthenticationServiceTest.java
@@ -1,87 +1,97 @@
-package com.fdmgroup.bookstore.test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+package com.fdmgroup.bookstore.service.test;
+
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 
 import com.fdmgroup.bookstore.data.UserRepository;
-import com.fdmgroup.bookstore.model.Book;
-import com.fdmgroup.bookstore.model.BookGenre;
-import com.fdmgroup.bookstore.model.Order;
 import com.fdmgroup.bookstore.model.User;
 import com.fdmgroup.bookstore.service.AuthenticationService;
 import com.fdmgroup.bookstore.service.UserNotFoundException;
 
 class AuthenticationServiceTest {
-
-	UserRepository userRepository;
-	AuthenticationService authenticationService;
-	 
-	 
+	
+	AuthenticationService authenticationservice;
+	User user1;
+	
+	@Mock
+	UserRepository userRepoMock;
+	
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+	
 	@BeforeEach
-	public void setup() {
+	void setUp() throws Exception {
 		
-		userRepository = mock(UserRepository.class);
-		authenticationService =new  AuthenticationService(userRepository);
-		
+		userRepoMock = mock(UserRepository.class);
+		authenticationservice = new AuthenticationService(userRepoMock);
 	}
 
-
 	@Test
-	void testAuthenticate() throws UserNotFoundException {
+	void testUserAuthentication() throws UserNotFoundException {
 		
-		List<Order> orders = new ArrayList<Order>();
-		orders.add(new Order(1,new Book(1,7.5,"Jolly","LLB",BookGenre.ACTION),1,LocalDateTime.now()));
-		User expectedUser=new User(1,"keshav","ganesh","keshavg","xyz","keshavg@gmail.com",orders);
+		String username = "Varsha";
+		String password = "abc";
+		user1 = new User(1, "varsha", "panchal", "var", "1234", "abc@gmail.com", null);
 		
-		when(userRepository.validate("keshavg","xyz")).thenReturn(true);
-		when(userRepository.findByUsername("keshavg")).thenReturn(new User(1,"keshav","ganesh","keshavg","xyz","keshavg@gmail.com",orders));
+		when(userRepoMock.validate(username, password)).thenReturn(true);
+		when(userRepoMock.findByUsername(username)).thenReturn(user1);
 		
-		User actualUser=authenticationService.authenticate("keshavg", "xyz");
+		assertEquals(user1,authenticationservice.authenticate(username, password));
+	}
 		
-		assertEquals(expectedUser,actualUser);
+	@Test
+	void testUserNotFoundException(){
+		
+		String username = "Varsha";
+		String password = "abc";
+		
+		when(userRepoMock.validate(username, password)).thenReturn(false);
+		
+		
+		try {
+			assertEquals(user1,authenticationservice.authenticate(username, password));
+			
+		} catch (UserNotFoundException e) {
+			
+		}
+		
 	}
 	
-	@Test
-	void testAuthenticate_userNotFoundException() throws UserNotFoundException {
-		Exception exception = assertThrows(UserNotFoundException.class, () -> {
-			User actualUser=authenticationService.authenticate("keshavg", "xyz");
-	    }); 
 	
-	    String expectedMessage = "User not Found";
-	    String actualMessage = exception.getMessage();
-	
-	    assertTrue(actualMessage.contains(expectedMessage));
-}
-
 	@Test
 	void testFindById() throws UserNotFoundException {
-		List<Order> orders = new ArrayList<Order>();
-		orders.add(new Order(1,new Book(1,7.5,"Jolly","LLB",BookGenre.ACTION),1,LocalDateTime.now()));
-		User expectedUser=new User(1,"keshav","ganesh","keshavg","xyz","keshavg@gmail.com",orders);
-		when(userRepository.findById(1)).thenReturn(expectedUser);
-		assertEquals(expectedUser,authenticationService.findById(1));		
+		
+		int uId =1;
+		user1 = new User(1, "varsha", "panchal", "var", "1234", "abc@gmail.com", null);
+		
+		when(userRepoMock.findById(uId)).thenReturn(user1);
+		
+		assertEquals(user1, authenticationservice.findById(uId));
 	}
+	
+	
 	
 	@Test
-	void testFindById_UserNotFoundException() throws UserNotFoundException {
-		Exception exception = assertThrows(UserNotFoundException.class, () -> {
-			User actualUser=authenticationService.findById(1);
-	    }); 
-	
-	    String expectedMessage = "User not Found";
-	    String actualMessage = exception.getMessage();
-	
-	    assertTrue(actualMessage.contains(expectedMessage));
-				
+	void testFindByIdException() throws UserNotFoundException {
+		
+		int uId =1;
+		user1 = new User(1, "varsha", "panchal", "var", "1234", "abc@gmail.com", null);
+		
+		when(userRepoMock.findById(uId)).thenReturn(null);
+		
+		try {
+			
+			assertEquals(user1, authenticationservice.findById(uId));
+		} catch (UserNotFoundException e) {
+			
+		}
 	}
-
 }
